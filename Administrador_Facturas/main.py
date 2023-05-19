5653cd3b0ad31421dd0672177e71aff6eee8143c
Daniel  Hernández <52589046+DanielHernandez627@users.noreply.github.com>
2023-05-18 20:37:57 -0500

    Modificaciones a valores unitarios

--- Administrador_Facturas/main.py
+++ Administrador_Facturas/main.py
@@ -20,6 +20,17 @@ def read_Categories():
     return rf.read_Json_Category()
 
 
+def on_modified_value(event):
+    amount = event.widget.get("1.0")
+    select_category = select_list_categories.get()
+    select_product = select_list_products.get()
+    price = rf.read_Json_Val_Product(select_category, select_product)
+    if amount != "\n":
+        amount_int = int(amount)
+        result = price * amount_int
+        lb2.config(text="Valor Producto: " + str(result))
+
+
 def on_select(event):
     products.clear()
     select = event.widget.get()
@@ -35,7 +46,7 @@ def on_select_products(event):
     select_category = select_list_categories.get()
     select_product = event.widget.get()
     price = rf.read_Json_Val_Product(select_category, select_product)
-    lb2.config(text="Valor Producto: "+str(price))
+    lb2.config(text="Valor Producto: " + str(price))
 
 
 # Interfaz
@@ -50,7 +61,15 @@ lb1.place(x=150, y=28)
 lb2 = tk.Label(window, text="Valor Producto: ")
 lb2.config(background="#AFEEEE")
 lb2.grid(column=0, row=0)
-lb2.place(x=345, y=110)
+lb2.place(x=475, y=110)
+lb3 = tk.Label(window, text="Cantidad Unidades: ")
+lb3.config(background="#AFEEEE")
+lb3.grid(column=0, row=0)
+lb3.place(x=345, y=85)
+text_box_cant = tk.Text(window, height=1, width=3)
+text_box_cant.place(x=385, y=110)
+text_box_cant.insert(tk.END, 1)
+text_box_cant.bind("<KeyRelease>", on_modified_value)
 select_list_categories = ttk.Combobox(window, values=read_Categories())
 select_list_categories.bind("<<ComboboxSelected>>", on_select)
 select_list_categories.place(x=25, y=110)
@@ -58,6 +77,8 @@ select_list_categories.current(0)
 select_list_products = ttk.Combobox(window, values=products)
 select_list_products.current(0)
 select_list_products.place(x=190, y=110)
+
+# Events
 select_list_products.bind("<<ComboboxSelected>>", on_select_products)
 
 # Diseño de la tabla
